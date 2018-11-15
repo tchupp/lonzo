2eb3a4b04fde6361b5879ec557d42431e4834674
theochupp <tclchiam@gmail.com>
2018-11-15 10:47:55 -0500

    Added the ability to parse expressions with division, multiplication, subtraction, and addition

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[macro_use] extern crate lalrpop_util;
+#[macro_use]
+extern crate lalrpop_util;
 
 lalrpop_mod!(pub calculator); // synthesized by LALRPOP
 
@@ -6,10 +7,48 @@ fn main() {
     println!("Hello, world!");
 }
 
-#[test]
-fn calculator1() {
-    assert!(calculator::TermParser::new().parse("22").is_ok());
-    assert!(calculator::TermParser::new().parse("(22)").is_ok());
-    assert!(calculator::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calculator::TermParser::new().parse("((22)").is_err());
+#[cfg(test)]
+mod tests {
+    use super::calculator;
+
+    #[test]
+    fn parser_can_handle_int_with_parentheses() {
+        assert_eq!(calculator::ExprParser::new().parse("22").unwrap(), 22);
+        assert_eq!(calculator::ExprParser::new().parse("(14)").unwrap(), 14);
+        assert_eq!(calculator::ExprParser::new().parse("((((7))))").unwrap(), 7);
+
+        assert!(calculator::ExprParser::new().parse("((91)").is_err());
+    }
+
+    #[test]
+    fn parser_can_handle_addition() {
+        assert_eq!(calculator::ExprParser::new().parse("11+1").unwrap(), 12);
+        assert_eq!(calculator::ExprParser::new().parse("(10  ) +  12").unwrap(), 22);
+
+        assert!(calculator::ExprParser::new().parse("1 + (21").is_err());
+    }
+
+    #[test]
+    fn parser_can_handle_subtraction() {
+        assert_eq!(calculator::ExprParser::new().parse("23-1").unwrap(), 22);
+        assert_eq!(calculator::ExprParser::new().parse("40   -  (  12)").unwrap(), 28);
+
+        assert!(calculator::ExprParser::new().parse("35 - (12").is_err());
+    }
+
+    #[test]
+    fn parser_can_handle_multiplication() {
+        assert_eq!(calculator::ExprParser::new().parse("11 * 2").unwrap(), 22);
+        assert_eq!(calculator::ExprParser::new().parse("3        * (7)").unwrap(), 21);
+
+        assert!(calculator::ExprParser::new().parse("(5 * 6").is_err());
+    }
+
+    #[test]
+    fn parser_can_handle_division() {
+        assert_eq!(calculator::ExprParser::new().parse("12 / 2").unwrap(), 6);
+        assert_eq!(calculator::ExprParser::new().parse("26        / (2)").unwrap(), 13);
+
+        assert!(calculator::ExprParser::new().parse("(5 / 1").is_err());
+    }
 }
